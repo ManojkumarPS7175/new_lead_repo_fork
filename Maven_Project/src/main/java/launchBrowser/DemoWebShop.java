6d2b1ee7ce1f6afbe78a007bcef2e23ccf233eec
manoj <manoj@Manu>
2025-07-02 13:35:13 +0530

    i have added launch url

---
+++ Maven_Project/src/main/java/launchBrowser/DemoWebShop.java
@@ -0,0 +1,20 @@
+package launchBrowser;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoWebShop {
+	@Test
+	
+	public void LaunchUrl(WebDriver driver) {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		driver.get("https://demowebshop.tricentis.com/login");
+	}
+
+}
