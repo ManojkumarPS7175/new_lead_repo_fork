6e628c6977436b004cab1afecfc3ac0856c4359e
ganes <ganes@DESKTOP-8AKJV26>
2025-07-02 13:35:29 +0530

    Launch Demo WebShop

---
+++ Maven_Project/src/main/java/launchBrowser/LaunchDemoWebShop.java
@@ -0,0 +1,20 @@
+package launchBrowser;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchDemoWebShop {
+
+	@Test
+	
+	public void DMS() {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://demowebshop.tricentis.com/");
+	}
+}
